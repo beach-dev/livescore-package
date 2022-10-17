2e648b83872fc6763ef52eddd2417e87ecad397e
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2022-10-18 02:32:47 +0300

    remove router

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,14 @@ import { defaultTheme } from "../src/theme";
 import { GlobalStyle } from "../src/theme/global.state";
 // pages
 import { LiveScore } from "../src/page";
-import { BrowserRouter } from "react-router-dom";
 
 
 export const LiveScoreApp = () => {
   return (
-    <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
         {/* router part  */}
         <LiveScore />
         <GlobalStyle />
       </ThemeProvider>
-    </BrowserRouter>
   );
 };
